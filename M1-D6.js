510ee201b3a254802f1d25e7232d03e2caf2a235
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-11-01 13:31:30 +0100

    JS Functions - Exercise 3

--- M1-D6.js
+++ M1-D6.js
@@ -106,6 +106,11 @@ console.log(whoIsBigger(5, 43))
     Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
 */
 
+splitMe = function (stringToSplit) {
+  return stringToSplit.split(" ")
+}
+console.log(splitMe("Rafael Neves Ferreira"))
+
 /* EXERCISE 4
     Write a function called deleteOne which receives a string and a boolean as parameters.
     If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
